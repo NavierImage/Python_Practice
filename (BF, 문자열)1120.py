b8ffb687698f624c3a729dd1dfbae43a4f134d62
larmor freq <100037095+cwlg102@users.noreply.github.com>
2022-05-02 22:37:26 +0900

    Update (BF, 문자열)1120.py

--- (BF, 문자열)1120.py
+++ (BF, 문자열)1120.py
@@ -1,6 +1,7 @@
 a, b = input().split()
 dif = abs(len(a) - len(b)) + 1
 rlt = []
+#어차피 붙일거는 긴거랑 똑같으니깐, 겹치는 문자가 제일 많은걸 잡아두면됨(안겹치는게 제일 적을때)
 if len(a) > len(b):
     for i in range(dif):
         dcnt = 0
