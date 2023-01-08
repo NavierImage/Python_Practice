5b3f7d1d91f4524f16223eff55143f38dc0201f5
larmor freq <100037095+cwlg102@users.noreply.github.com>
2023-01-08 23:22:30 +0900

    Add files via upload

--- (구현,수학)15685드래곤커브.py
+++ (구현,수학)15685드래곤커브.py
@@ -1,5 +1,4 @@
 import sys
-import math
 
 n = int(sys.stdin.readline().rstrip())
 dragon_curve = []
@@ -15,22 +14,17 @@ dy = [0, -1, 0, 1]
 for curve in dragon_curve:
     x, y, d, g = curve
     g += 1
-    curve_c = []
-    for i in range(g):
-        if i == 0:
-            #모든 케이스에 대하여 중심이 (0, 0)이라고 생각함
-            curve_c.append((0, 0))
-            curve_c.append((dy[d], dx[d]))
-
-        else:
-            st_y, st_x = curve_c[-1]
-            for j in range(len(curve_c)-2, -1, -1):
-                y1, x1 = curve_c[j]
-                movedY, movedX = y1-st_y, x1-st_x #평행이동 (커브의 끝점을 (0, 0)으로)
-                new_x, new_y = -movedY, movedX #90도 회전 변환 -> (x, y) 넣은게 (-y, x)로.
-                new_y += st_y
-                new_x += st_x
-                curve_c.append((new_y, new_x))
+    curve_c = [(0, 0), (dy[d], dx[d])]
+    for i in range(g-1):
+        #모든 케이스에 대하여 중심이 (0, 0)이라고 생각함
+        st_y, st_x = curve_c[-1]
+        for j in range(len(curve_c)-2, -1, -1):
+            y1, x1 = curve_c[j]
+            movedY, movedX = y1-st_y, x1-st_x #평행이동 (커브의 끝점을 (0, 0)으로)
+            new_x, new_y = -movedY, movedX #90도 회전 변환 -> (x, y) 넣은게 (-y, x)로.
+            new_y += st_y
+            new_x += st_x
+            curve_c.append((new_y, new_x))
     for i in range(len(curve_c)):
         b, a = curve_c[i]
         arr[b+y][a+x] = 1
